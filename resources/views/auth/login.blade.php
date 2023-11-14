1371c135125a4d4f5a4b1aa29f7ccccc4408339d
Your NameMJunaidAfzal <mjunaidafzal395@gmail.com>
2023-11-15 00:18:19 +0500

    junaid

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,21 +18,8 @@ Login/Register
                     <div class="col-md-6">
                         <div class="auth-form-cotainer">
                             <div class="w-100">
-                                <ul class="nav nav-pills mb-5" id="pills-tab" role="tablist">
-                                    <li class="nav-item" role="presentation">
-                                        <button class="nav-link active" id="login-tab" data-bs-toggle="pill"
-                                            data-bs-target="#login" type="button" role="tab" aria-controls="login"
-                                            aria-selected="true">Login</button>
-                                    </li>
-                                    <li class="nav-item" role="presentation">
-                                        <button class="nav-link" id="register-tab" data-bs-toggle="pill"
-                                            data-bs-target="#register" type="button" role="tab" aria-controls="register"
-                                            aria-selected="false">Register</button>
-                                    </li>
-                                </ul>
                                 <div class="tab-content" id="pills-tabContent">
-                                    <div class="tab-pane fade show active" id="login" role="tabpanel"
-                                        aria-labelledby="login-tab" tabindex="0">
+                                    <div id="login">
                                         <form method="POST" action="{{ route('login') }}" class="auth-form w-100">
                                             @csrf
                                             <div class="mb-3">
@@ -68,68 +55,6 @@ Login/Register
                                             </div>
                                             <button type="submit"
                                                 class="btn btn-primary w-100 fw-bold mt-4">Login</button>
-                                        </form>
-                                    </div>
-                                    <div class="tab-pane fade" id="register" role="tabpanel"
-                                        aria-labelledby="register-tab" tabindex="0">
-                                      <form method="POST" action="{{ route('register') }}" class="auth-form w-100">
-                                        @csrf
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="mb-3">
-                                                        <label for="exampleInputEmail1" class="form-label">First
-                                                            Name</label>
-                                                            <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
-                                                            @error('first_name')
-                                                            <span class="invalid-feedback" role="alert">
-                                                                <strong>{{ $message }}</strong>
-                                                            </span>
-                                                        @enderror
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="mb-3">
-                                                        <label for="exampleInputEmail1" class="form-label">Last
-                                                            Name</label>
-                                                            <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
-                                                            @error('last_name')
-                                                            <span class="invalid-feedback" role="alert">
-                                                                <strong>{{ $message }}</strong>
-                                                            </span>
-                                                        @enderror
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="exampleInputEmail2" class="form-label">Email</label>
-                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                                @error('email')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="exampleInputPassword1" class="form-label">Password</label>
-                                                <div class="position-relative pass-eye">
-                                                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                                    <i class="fa-solid fa-eye toggle-password"></i>
-                                                       @error('password')
-                                                        <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
-                                                        </span>
-                                                    @enderror
-                                                </div>
-                                            </div>
-                                            {{-- <div class="mb-3">
-                                                <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                                                <div class="position-relative pass-eye">
-                                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                                    <i class="fa-solid fa-eye toggle-password"></i>
-                                                </div>
-                                            </div> --}}
-                                            <button type="submit" class="btn btn-primary w-100 fw-bold mt-4">Register
-                                                Now</button>
                                         </form>
                                     </div>
                                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,118 @@
-@extends('layouts.app')
 
+@extends('layouts.auth-scaffold')
+@push('title')
+Login/Register
+@endpush
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <main>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+        <section class="auth">
+            <div class="container-fluid px-0">
+                <div class="row g-0">
+                    <div class="col-md-6 d-md-block d-none">
+                        <div class="side-img">
+                            <img src="{{asset('assets/imgs/side-img.png')}}" alt="">
                         </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="auth-form-cotainer">
+                            <div class="w-100">
+                                <div class="tab-content" id="pills-tabContent">
+                                    <div id="register">
+                                      <form method="POST" action="{{ route('register') }}" class="auth-form w-100">
+                                        @csrf
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="mb-3">
+                                                        <label for="exampleInputEmail1" class="form-label">First
+                                                            Name</label>
+                                                            <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}"  autocomplete="first_name" autofocus>
+                                                            @error('first_name')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="mb-3">
+                                                        <label for="exampleInputEmail1" class="form-label">Last
+                                                            Name</label>
+                                                            <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}"  autocomplete="last_name" autofocus>
+                                                            @error('last_name')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail2" class="form-label">Email</label>
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email">
+                                                @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                                <div class="position-relative pass-eye">
+                                                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="new-password">
+                                                    <i class="fa-solid fa-eye toggle-password"></i>
+                                                       @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="password-confirm" class="form-label">Confirm Password</label>
+                                                <div class="position-relative pass-eye">
+                                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
+                                                    <i class="fa-solid fa-eye toggle-password"></i>
+                                                </div>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary w-100 fw-bold mt-4">Register
+                                                Now</button>
+                                        </form>
+                                    </div>
+                                </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="auth-policy">
+                                <img src="{{asset('assets/imgs/logo.png')}}" width="120" alt="">
+                                <p class="mb-0">All trademarks are the property of Trades Accepted or a related company
+                                    or a licensor unless otherwise noted. ©2023 Trades Accepted. All rights reserved.
+                                </p>
                             </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        </section>
+    </main>
 @endsection
+@push('scripts')
+    <script>
+        $(".toggle-password").click(function () {
+            $(this).toggleClass("fa-eye fa-eye-slash");
+            input = $(this).parent().find("input");
+            if (input.attr("type") == "password") {
+                input.attr("type", "text");
+            } else {
+                input.attr("type", "password");
+            }
+        });
+    </script>
+@endpush
+
+
+
