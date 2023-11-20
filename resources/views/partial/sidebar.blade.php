d18d3c5e063bd012ae12fdfba76cbbb0f9d882c5
Your NameMJunaidAfzal <mjunaidafzal395@gmail.com>
2023-11-20 22:11:47 +0500

    junaid

---
+++ resources/views/partial/sidebar.blade.php
@@ -0,0 +1,35 @@
+
+<aside>
+    <div class="d-flex flex-column justify-content-between h-100">
+        <ul class="list-unstyled side-nav">
+            <li>
+                <a href="javascript:;">
+                    <img src="./assets/imgs/fi_layout.svg" alt="">
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <img src="./assets/imgs/fi_align-left.svg" alt="">
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <img src="./assets/imgs/fi_bell-1.svg" alt="">
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <img src="./assets/imgs/fi_message-square-1.svg" alt="">
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <img src="./assets/imgs/fi_settings.svg" alt="">
+                </a>
+            </li>
+        </ul>
+        <a href="javascript:;" class="d-block">
+            <img src="./assets/imgs/fi_log-out.svg" alt="">
+        </a>
+    </div>
+</aside>
---
+++ resources/views/partial/styles.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
