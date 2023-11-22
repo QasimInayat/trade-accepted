78f01ab8b726c70b76608f8957d3f35a1502c4ca
Fahad khalid <mfahadkhalid5002@gmail.com>
2023-11-22 22:52:44 +0500

    fahad

--- resources/views/partial/scripts.blade.php
+++ resources/views/partial/scripts.blade.php
@@ -1,4 +1,3 @@
-
 <script src="{{asset('assets/js/jquery.min.js')}}"></script>
 <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
@@ -22,8 +21,3 @@
         toastr.error("{{ session('error') }}")
     @endif
   </script>
-
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
