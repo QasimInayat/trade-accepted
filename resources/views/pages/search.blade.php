37c2a458ef6b922e9fb6715f2ac393d78eb47f5b
Your NameMJunaidAfzal <mjunaidafzal395@gmail.com>
2024-01-13 23:49:20 +0500

    junaid

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -18,7 +18,7 @@ Search
                         {{-- <p class="view-all mb-0 text-end">View All</p> --}}
                     </div>
                 </div>
-
+{{--
                 <div class="row mb-5">
                     @forelse($searchVehicles as $vehicle)
                     <div class="col-lg-3 col-md-6 mb-3">
@@ -44,7 +44,7 @@ Search
 
                                                 <i style="font-size: 10px;" class="fa fa-bookmark"></i>
                                                                                                 {{-- <img src="{{asset('assets/imgs/fi_bookmark.svg')}}" alt=""> --}}
-                                            </span>
+                                             {{-- </span>
                                                 @else
                                                 @endif
                                         </div>
@@ -52,7 +52,7 @@ Search
                                 </div>
                                </a>
                                 <div class="d-flex justify-content-between align-items-center mt-2">
-                                    <h4 class="mb-0">${{ $vehicle->price }}</h4>
+                                    <h4 class="mb-0">${{ number_format($vehicle->price) }}</h4>
                                     <p class="mb-0">{{ $vehicle->country_id }} . {{ $vehicle->city_id }}</p>
                                 </div>
 
@@ -62,7 +62,80 @@ Search
                     @empty
                     <h4><b>DATA NOT ADDED</b></h4>
                     @endforelse
+
+
+                </div>  --}}
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-8">
+                        <div style="border:groove 1px lightgrey;background-color:rgb(245, 242, 242)" class="card">
+                            <div class="card-body">
+                              <div class="row">
+                                <div class="col-md-1">
+                                    <small style="font-size: 13px">Sort By:</small>
+                                </div>
+                                <div class="col-md-5">
+                                    <select name="" id="" class="form-control p-2">
+                                        <option value="Select Vehicle">Select Vehicle</option>
+                                        <option value="">Toyota</option>
+                                        <option value="">Honda</option>
+                                        <option value="">Suzuki</option>
+                                    </select>
+                                </div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mt-5" ></div>
+                    <div class="col-md-8">
+                    @forelse($searchVehicles as $vehicle)
+                        <div style="border:groove 1px lightgrey;background-color:rgb(245, 242, 242)" class="card mt-4">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <a href="{{ route('detail',$vehicle->slug) }}"><img class="w-100" style="height: 150px;" src="{{ asset('upload/vehicle/'.mainImage($vehicle->id)) }}" alt=""></a>
+                                    </div>
+                                        <div class="col-md-6">
+                                            <a href="{{ route('detail',$vehicle->slug) }}"><h4 class="text-primary" style="font-size:18px;margin-left:15px;margin-top:5px">{{ $vehicle->title }}</h4></a>
+                                            <span style="font-size: 13px;margin-left:15px">{{ $vehicle->country_id }} . {{ $vehicle->city_id }}</span><br>
+                                            <br>
+                                            <span  style="font-size: 14px;margin-left:15px">{{ $vehicle->year }} | {{ $vehicle->mileage }} | {{ $vehicle->fuel }} | {{ $vehicle->trim }}</span>
+                                            <br>
+                                            <span style="font-size: 11px;margin-left:15px">Updated about 2 hours ago</span>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <span style="font-size:19px;margin-left:60px"><b>${{ number_format($vehicle->price) }}</b></span>
+                                            @auth
+                                                <span>
+                                                    @php $countFavourite = 0 @endphp
+                                                            @if (Auth::check())
+                                                                @php
+                                                                    $countFavourite = App\Models\Favourite::countFavourite($vehicle['id']);
+                                                                @endphp
+                                                            @endif
+                                                            <a style="cursor: pointer;"  data-vehicleid="{{ $vehicle->id }}" class="ms-1 update-favourite">
+                                                                @if ($countFavourite > 0)
+                                                                <i style="font-size: 13px;  color: red; margin-bottom: 7px;" class="fa fa-bookmark"></i>
+                                                                @else
+                                                                <i style="font-size: 13px;  color: red; margin-bottom: 7px" class="fa fa-bookmark-o"></i>
+                                                                    @endif
+                                                            </a>
+                                                    {{-- <img src="{{asset('assets/imgs/fi_bookmark-red.svg')}}" alt=""> --}}
+                                                </span>
+                                            @endauth
+                                            <button style="font-size:10px;margin-left:45px;margin-top:10px" class="btn btn-primary"> <i class="fa fa-phone"></i>   Show Phone No.</button>
+                                        </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        @empty
+                    <h4><b>DATA NOT ADDED</b></h4>
+                    @endforelse
+                    </div>
+
                 </div>
+
             </div>
         </div>
     </main>
@@ -125,3 +198,58 @@ Search
     </div>
 
 @endsection
+@push('scripts')
+<script>
+    var user_id = "{{ Auth::id() }}";
+    $(document).ready(function() {
+        $('.update-favourite').click(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            var vehicle_id = $(this).data('vehicleid');
+            $.ajax({
+                type: 'POST',
+                url: '{{ route('favourite.store') }}',
+                data: {
+                    vehicle_id: vehicle_id,
+                    user_id: user_id
+                },
+                beforeSend : function(response){
+                $('.loader').show();
+                },
+                success:function(response){
+                    if(response.action == 'type'){
+                        $('.loader').hide();
+                        toastr.error(response.error);
+                    }
+                    else if(response.action == 'add'){
+                        $('.loader').hide();
+                        $('a[data-vehicleid='+vehicle_id+']').html(`<i style="font-size: 13px;  color: red; margin-bottom: 7px" class="fa fa-bookmark"></i>`);
+                        toastr.success(response.message);
+
+                    } else if (response.action == 'remove') {
+                        $('.loader').hide();
+                        $('a[data-vehicleid=' + vehicle_id + ']').html(
+                            `<i style="font-size: 13px;  color: red; margin-bottom: 7px" class="fa fa-bookmark-o"></i>`);
+                        toastr.success(response.message);
+                    }
+                }
+            });
+        });
+    });
+</script>
+<script>
+    function myFunction() {
+        toastr.success('Text copied');
+      var copyText = document.getElementById("myInput");
+      copyText.select();
+      copyText.setSelectionRange(0, 99999);
+      navigator.clipboard.writeText(copyText.value);
+
+      var tooltip = document.getElementById("myTooltip");
+      tooltip.innerHTML = "Copied: " + copyText.value;
+    }
+    </script>
+@endpush
