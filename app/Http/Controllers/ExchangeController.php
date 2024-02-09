376033759aaad54873c6f82ad2f00d88249aa14c
Fahad khalid <mfahadkhalid5002@gmail.com>
2024-02-09 23:36:32 +0500

    trade

--- app/Http/Controllers/ExchangeController.php
+++ app/Http/Controllers/ExchangeController.php
@@ -2,20 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Make;
 use App\Models\Vehicle;
+use Illuminate\Http\Request;
 
 class ExchangeController extends Controller
 {
-    public function exchange(){
-        $data ['title'] = 'Exchange your cars';
+    public function exchange(Request $request){
+        $data ['title'] = 'Ex your cars';
         $data ['heading'] = 'Exchange your cars';
-        $data ['vehicles'] = Vehicle::get();
+        $data ['vehicles'] = Vehicle::where('status','1')->get();
+        $data['makes'] = Make::get();
+        if(isset($request->vehicle_id)){
+            $vehicle = Vehicle::where('id',$request->vehicle_id)->first();
+            // dd(array_values($request->make));
+
+            $query = new Vehicle();
+            if(isset($request->make)){
+                $query = $query->whereIn('make_id',$request->make);
+            }
+            if(isset($request->model)){
+                $query = $query->whereIn('model_id',$request->model);
+            }
+            if(isset($request->year)){
+                $query = $query->whereIn('year',$request->year);
+            }
+            if(isset($request->transmission)){
+                $query = $query->whereIn('transmission',$request->transmission);
+            }
+
+            $query = $query->get();
+
+            dd($query);
+
+        }
         return view('pages.exchange',$data);
     }
     public function exchangeVehicle($id){
         $data['vehicle'] = Vehicle::where('id',$id)->firstorfail();
         $view = view('pages.exchange-vehicle',$data);
         return response(['success' => true, 'data' => $view->render()]);
+    }
+    public function store(Request $request){
     }
 }
--- resources/views/pages/exchange.blade.php
+++ resources/views/pages/exchange.blade.php
@@ -7,143 +7,168 @@
         <div class="content pe-2">
             <div class="p-1">
 
-                <div style="background-image: url({{ asset('assets/imgs/bg-image.jpg') }})">
+                <form action="{{ route('exchange') }}" method="GET">
+                    <div style="background-image: url({{ asset('assets/imgs/bg-image.jpg') }})">
 
-                    <div class="text-center p-4">
-                        <h1 class="text-white">Exchange Your Cars </h1>
-                        {{-- <p class="text-white">With thousands of cars, we have just the right one for you</p> --}}
-                        <div class="row">
-                            <div class="col-md-2"></div>
+                        <div class="text-center p-4">
+                            <h1 class="text-white">Exchange Your Cars </h1>
+                            {{-- <p class="text-white">With thousands of cars, we have just the right one for you</p> --}}
+                            <div class="row">
+                                <div class="col-md-2"></div>
 
-                            <div class="col-md-8">
-                                <div class="mb-2">
-                                    <select name="" id="vehicle-select" class="form-control">
-                                        <option value="">Please Select</option>
-                                        @forelse ($vehicles as $item)
-                                            <option value="{{ $item->id }}">{{ $item->title }}</option>
-                                        @empty
-                                            Not Data Added
-                                        @endforelse
-                                    </select>
+                                <div class="col-md-8">
+                                    <div class="mb-2">
+
+                                        <select name="vehicle_id" id="vehicle-select" required class="form-control">
+                                            <option value="">Please Select</option>
+                                            @forelse ($vehicles as $item)
+                                                <option value="{{ $item->id }}">{{ $item->title }}</option>
+                                            @empty
+                                                Not Data Added
+                                            @endforelse
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="col-md-2"></div>
+                                <div class="col-md-2"></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div id="exchange_vehicle">
+                    <div id="exchange_vehicle">
 
 
 
-                </div>
+                    </div>
 
-                <div class="row p-4">
-                    <div class="col-md-4">
-                        <div class="card mt-4">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="text-center">
-                                            <h4 class="text-primary"><b>Title</b></h4>
+                    <div class="row p-4">
+                        <div class="col-md-4">
+                            <div class="card mt-4">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="text-center">
+                                                <h4 class="text-primary"><b>Title</b></h4>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Make</b></label>
+                                            <select name="make[]" class="form-control" required>
+                                                <option value="">Please Select</option>
+                                                @forelse ($makes as $make)
+                                                    <option value="{{ $make->id }}">{{ $make->name }}</option>
+                                                @empty
+                                                    
+                                                @endforelse
+                                            </select>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Model</b></label>
+                                            <input type="text" name="model[]" class="form-control mt-2" required>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Year</b></label>
+                                            <input type="text" name="year[]" class="form-control mt-2" required>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Transmission</b></label>
+                                            <input type="text" name="transmission[]" class="form-control mt-2" required>
                                         </div>
                                     </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Make</b></label>
-                                        <input type="text" name="make[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Model</b></label>
-                                        <input type="text" name="model[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Year</b></label>
-                                        <input type="text" name="year[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Transmission</b></label>
-                                        <input type="text" name="transmission[]" class="form-control mt-2">
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-md-4">
-                        <div class="card mt-4">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="text-center">
-                                            <h4 class="text-primary"><b>Title</b></h4>
+                        <div class="col-md-4">
+                            <div class="card mt-4">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="text-center">
+                                                <h4 class="text-primary"><b>Title</b></h4>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Make</b></label>
+                                            <select name="make[]" class="form-control" required>
+                                                <option value="">Please Select</option>
+                                                @forelse ($makes as $make)
+                                                    <option value="{{ $make->id }}">{{ $make->name }}</option>
+                                                @empty
+                                                    
+                                                @endforelse
+                                            </select>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Model</b></label>
+                                            <input type="text" name="model[]" class="form-control mt-2" required>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Year</b></label>
+                                            <input type="text" name="year[]" class="form-control mt-2" required>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Transmission</b></label>
+                                            <input type="text" name="transmission[]" class="form-control mt-2" required>
                                         </div>
                                     </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Make</b></label>
-                                        <input type="text" name="make[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Model</b></label>
-                                        <input type="text" name="model[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Year</b></label>
-                                        <input type="text" name="year[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Transmission</b></label>
-                                        <input type="text" name="transmission[]" class="form-control mt-2">
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
 
 
-                    <div class="col-md-4">
-                        <div class="card mt-4">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="text-center">
-                                            <h4 class="text-primary"><b>Title</b></h4>
+                        <div class="col-md-4">
+                            <div class="card mt-4">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="text-center">
+                                                <h4 class="text-primary"><b>Title</b></h4>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Make</b></label>
+                                            <select name="make[]" class="form-control" required>
+                                                <option value="">Please Select</option>
+                                                @forelse ($makes as $make)
+                                                    <option value="{{ $make->id }}">{{ $make->name }}</option>
+                                                @empty
+                                                    
+                                                @endforelse
+                                            </select>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Model</b></label>
+                                            <input type="text" name="model[]" class="form-control mt-2" required>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Year</b></label>
+                                            <input type="text" name="year[]" class="form-control mt-2" required>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label class="text-primary mt-3"><b>Transmission</b></label>
+                                            <input type="text" name="transmission[]" class="form-control mt-2" required>
                                         </div>
                                     </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Make</b></label>
-                                        <input type="text" name="make[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Model</b></label>
-                                        <input type="text" name="model[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Year</b></label>
-                                        <input type="text" name="year[]" class="form-control mt-2">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <label class="text-primary mt-3"><b>Transmission</b></label>
-                                        <input type="text" name="transmission[]" class="form-control mt-2">
-                                    </div>
                                 </div>
                             </div>
                         </div>
+
+
                     </div>
+                    <div class="row p-4" style="margin-top: -30px;" id="append_card">
 
-
-                </div>
-                <div class="row p-4" style="margin-top: -30px;" id="append_card">
-
-                </div>
-                <div style="justify-content: center; text-align:center;">
-                    <div id="add_card" style="width: 60px; " class="btn btn-primary mt-3">
-                        <i class="fa fa-plus"></i>
                     </div>
-                </div>
-                <div class="col-md-12 mt-4 text-center mb-3">
-                    <button style="padding-left: 50px;padding-right:50px" class="btn btn-primary">Search</button>
-                </div>
+                    <div style="justify-content: center; text-align:center;">
+                        <div id="add_card" style="width: 60px; " class="btn btn-primary mt-3">
+                            <i class="fa fa-plus"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mt-4 text-center mb-3">
+                        <button style="padding-left: 50px;padding-right:50px" type="submit"
+                            class="btn btn-primary">Search</button>
+                    </div>
+                </form>
 
             </div>
         </div>
@@ -166,19 +191,19 @@
                     "</div>" +
                     "<div class='col-md-12'>" +
                     "<label class='text-primary mt-3'><b>Make</b></label>" +
-                    "<input type='text' name='make[]' class='form-control mt-2'>" +
+                    "<input type='text' name='make[]' class='form-control mt-2' required>" +
                     "</div>" +
                     "<div class='col-md-12'>" +
                     "<label class='text-primary mt-3'><b>Model</b></label>" +
-                    "<input type='text' name='model[]' class='form-control mt-2'>" +
+                    "<input type='text' name='model[]' class='form-control mt-2' required>" +
                     "</div>" +
                     "<div class='col-md-12'>" +
                     "<label class='text-primary mt-3'><b>Year</b></label>" +
-                    "<input type='text' name='year[]' class='form-control mt-2'>" +
+                    "<input type='text' name='year[]' class='form-control mt-2' required>" +
                     "</div>" +
                     "<div class='col-md-12'>" +
                     "<label class='text-primary mt-3'><b>Transmission</b></label>" +
-                    "<input type='text' name='transmission[]' class='form-control mt-2'>" +
+                    "<input type='text' name='transmission[]' class='form-control mt-2' required>" +
                     "</div>" +
                     "</div>" +
                     "</div>" +
