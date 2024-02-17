3475cf3a9d9a71589ed35e9786c48a235663d785
Your NameMJunaidAfzal <mjunaidafzal395@gmail.com>
2024-02-17 23:29:34 +0500

    junaid

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -145,15 +145,15 @@ input[type="range"]::-moz-range-thumb {
                             <div class="separator"></div>
                             <div class="field">
                               <span>Max</span>
-                              <input type="number" name="max" class="input-max" value="{{ request('max') ?? 100000 }}">
+                              <input type="number" name="max" class="input-max" value="{{ request('max') ?? 10000000 }}">
                             </div>
                           </div>
                           <div class="slider">
                             <div class="progress"></div>
                           </div>
                           <div class="range-input">
-                            <input type="range" name="min" class="range-min" min="0" max="100000" value="{{ request('min') ?? 0 }}" step="100">
-                            <input type="range" name="max"  class="range-max" min="0" max="100000" value="{{ request('max') ?? 100000 }}" step="100">
+                            <input type="range" name="min" class="range-min" min="0" max="10000000" value="{{ request('min') ?? 0 }}" step="100">
+                            <input type="range" name="max"  class="range-max" min="0" max="10000000" value="{{ request('max') ?? 10000000 }}" step="100">
                           </div>
                         </div>
                       </div>
